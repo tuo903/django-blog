6931d5a28617a6ab4cf2c397019cc039e0271cd2
Tuo <tuo903@hotmail.com>
2018-03-09 20:03:41 +0800

    新增博客功能

--- tblog/views.py
+++ tblog/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,get_object_or_404
+from .models import BlogArticles
+from django.http import Http404
 # Create your views here.
+
+def blog_title(request):
+	blogs=BlogArticles.objects.all()
+	return render(request,'tblog/titles.html',{"blogs":blogs})
+
+def blog_article(request,article_id):
+	# article=BlogArticles.objects.get(id=article_id)
+
+	article=BlogArticles.objects.get(id=article_id)
+	pub=article.publish
+  
+	return render(request,'tblog/content.html',{"article":article,'publish':pub})
