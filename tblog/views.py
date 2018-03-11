1422afa35f7b29d97b1727cbb8deffcb31a1b5ef
tuo903 <ooing@126.com>
2018-03-11 10:13:58 +0800

    调整报错提示

--- tblog/views.py
+++ tblog/views.py
@@ -9,8 +9,9 @@ def blog_title(request):
 
 def blog_article(request,article_id):
 	# article=BlogArticles.objects.get(id=article_id)
-
-	article=BlogArticles.objects.get(id=article_id)
-	pub=article.publish
-  
+	try:
+		article=get_object_or_404(BlogArticles,pk=article_id)
+		pub=article.publish
+	except BlogArticles.DoesNotExist:
+		raise Http404("No MyModel matches the given query.")
 	return render(request,'tblog/content.html',{"article":article,'publish':pub})
